dda32e837fe828f600698fdb7e943a046b22d71b
VladZhidik <vlados72002@mail.ru>
2023-04-22 21:17:39 +0300

    task #1 in lesson 8

---
+++ Py88_8/class_auto.py
@@ -0,0 +1,15 @@
+class Auto:
+    brand = input(str)
+    age = input(int)
+    mark = input(str)
+    weight = 3
+    color = 'Black'
+
+    def move(self):
+        print('Move')
+
+    def birthday(self, age):
+        print(age + 1)
+
+    def stop(self):
+        print('Stop')
